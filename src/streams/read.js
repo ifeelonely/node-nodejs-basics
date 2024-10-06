a477bb9ca0f6ace9a88a5a191b7a3d9bb8271f7b
SmellyFinger <kooolyan123@mail.ru>
2024-10-06 12:00:13 +0300

    feat: implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import url from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filenameToRead = 'fileToRead.txt';
+  const filePath = path.join(__dirname, 'files', filenameToRead);
+  const readStream = fs.createReadStream(filePath);
+
+  readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(`${chunk.reverse()}\n`);
+      callback();
+    },
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
