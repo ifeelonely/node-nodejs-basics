9c7f0ceeeec55fced7657ceabe5492e13f2dafab
SmellyFinger <kooolyan123@mail.ru>
2024-10-07 17:22:23 +0300

    feat: implement wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { cpus } from 'node:os';
+import { Worker } from 'node:worker_threads';
+import path from 'node:path';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numOfCpus = +cpus().length;
+  const workerPath = path.join(__dirname, 'worker.js');
+  const promises = [];
+
+  for (let i = 0; i < numOfCpus; i++) {
+    promises.push(
+      new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: i + 10 });
+
+        worker.on('message', (data) => {
+          resolve({ status: 'resolved', data: data });
+        });
+
+        worker.on('error', (err) => {
+          resolve({ status: 'error', data: null });
+        });
+      })
+    );
+  }
+
+  Promise.all(promises).then((results) => {
+    console.log(results);
+  });
 };
 
 await performCalculations();
